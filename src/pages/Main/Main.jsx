726797ea5b35db7fab1104a55677f0b9f4ab45aa
Koddlisnk <olecyanikulina@gmail.com>
2025-10-20 17:29:22 +0300

    add filtering by categories

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,77 +1,104 @@
-import { useEffect, useState } from "react";
+import { use, useEffect, useState } from "react";
 import NewsBanner from "../../components/NewsBanner/NewsBanner";
 import styles from "./styles.module.css";
-import { getNews } from "../../api/apiNews";
+import { getNews, getCategories } from "../../api/apiNews";
 import NewsList from "../../components/NewsList/NewsList";
 import Skeleton from "../../components/Skeleton/Skeleton";
+import Categories from "../../components/Categories/Categories";
 import { Pagination as MuiPagination, Stack } from "@mui/material";
 
 const Main = () => {
-  const [news, setNews] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const [currentPage, setCurrentPage] = useState(1);
-  const totalPages = 10;
-  const pageSize = 10;
+    const [news, setNews] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [categories, setCategories] = useState([]);
+    const [selectedCategory, setSelectedCategory] = useState("All");
+    const totalPages = 10;
+    const pageSize = 10;
 
-  const fetchNews = async (currentPage) => {
-    try {
-      setIsLoading(true);
-      const response = await getNews(currentPage, pageSize);
-      setNews(response.news);
-    } catch (error) {
-      console.log(error);
-    } finally {
-      setIsLoading(false);
-    }
-  };
+    const fetchNews = async (currentPage) => {
+        try {
+            setIsLoading(true);
+            const response = await getNews({
+                page_number: currentPage,
+                page_size: pageSize,
+                category: selectedCategory === "All" ? null : selectedCategory,
+            });
+            setNews(response.news);
+        } catch (error) {
+            console.log(error);
+        } finally {
+            setIsLoading(false);
+        }
+    };
 
-  useEffect(() => {
-    fetchNews(currentPage);
-  }, [currentPage]);
+    const fetchCategories = async () => {
+        try {
+            const response = await getCategories();
+            setCategories(["All", ...response.categories]);
+        } catch (error) {
+            console.log(error);
+        }
+    };
 
-  const handleChangePage = (_evt, page) => setCurrentPage(page);
+    useEffect(() => {
+        fetchCategories();
+    }, []);
 
-  return (
-    <main className={styles.main}>
-      {news.length > 0 && !isLoading ? (
-        <NewsBanner item={news[0]} />
-      ) : (
-        <Skeleton type="banner" count={1} />
-      )}
+    useEffect(() => {
+        fetchNews(currentPage);
+    }, [currentPage, selectedCategory]);
 
-      <Stack alignItems="center" sx={{ my: 2 }}>
-        <MuiPagination
-          count={totalPages}
-          page={currentPage}
-          onChange={handleChangePage}
-          disabled={isLoading}
-          shape="rounded"
-          color="primary"
-          siblingCount={1}
-          boundaryCount={1}
-        />
-      </Stack>
+    const handleChangePage = (_evt, page) => setCurrentPage(page);
 
-      {!isLoading ? (
-        <NewsList news={news} />
-      ) : (
-        <Skeleton type="item" count={10} />
-      )}
+    return (
+        <>
+            <main className={styles.main}>
+                <Categories
+                    categories={categories}
+                    setSelectedCategory={setSelectedCategory}
+                    selectedCategory={selectedCategory} 
+                />
+                {news.length > 0 && !isLoading ? (
+                    <NewsBanner item={news[0]} />
+                ) : (
+                    <Skeleton type="banner" count={1} />
+                )}
 
-      <Stack alignItems="center" sx={{ my: 2 }}>
-        <MuiPagination
-          count={totalPages}
-          page={currentPage}
-          onChange={handleChangePage}
-          disabled={isLoading}
-          shape="rounded"
-          color="primary"
-          siblingCount={1}
-          boundaryCount={1}
-        />
-      </Stack>
-    </main>
-  );
+                <Stack alignItems="center" sx={{ my: 2 }}>
+                    <MuiPagination
+                        count={totalPages}
+                        page={currentPage}
+                        onChange={handleChangePage}
+                        disabled={isLoading}
+                        shape="rounded"
+                        color="primary"
+                        siblingCount={1}
+                        boundaryCount={1}
+                    />
+                </Stack>
+
+                {!isLoading ? (
+                    <NewsList news={news} />
+                ) : (
+                    <Skeleton type="item" count={10} />
+                )}
+
+                <Stack alignItems="center" sx={{ my: 2 }}>
+                    <MuiPagination
+                        count={totalPages}
+                        page={currentPage}
+                        onChange={handleChangePage}
+                        disabled={isLoading}
+                        shape="rounded"
+                        color="primary"
+                        siblingCount={1}
+                        boundaryCount={1}
+                    />
+                </Stack>
+            </main>
+        </>
+    );
 };
 
 export default Main;
